59bd6bac361c87752d6d17e7f3c9278007949462
zlkj <zlkj@zlkjdeMac-mini.local>
2017-02-09 14:43:16 +0800

    Initial Commit

---
+++ CRFN/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CRFN
+//
+//  Created by zlkj on 2017/2/9.
+//  Copyright © 2017年 zlkj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CRFN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CRFN
+//
+//  Created by zlkj on 2017/2/9.
+//  Copyright © 2017年 zlkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
